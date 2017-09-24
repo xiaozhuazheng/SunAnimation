ba94dc2556398a8247d665c57edd09a7beab805a
azheng <735481105@qq.com>
2017-09-24 23:25:04 +0800

    开发完成
    仿华为天气应用日落日出动画。

--- app/src/main/java/com/example/sunanimation/MainActivity.java
+++ app/src/main/java/com/example/sunanimation/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.sunanimation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText mET;
+    private Button mBegin;
+    private SunView mSunView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mET = (EditText) findViewById(R.id.time_et);
+        mBegin = (Button) findViewById(R.id.set_bt);
+        mSunView = (SunView) findViewById(R.id.sun);
+        mBegin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.set_bt:
+                String time = String.valueOf(mET.getText());
+                mSunView.setTime(time);
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/sunanimation/SunView.java
+++ app/src/main/java/com/example/sunanimation/SunView.java
@@ -1,11 +1,15 @@
 package com.example.sunanimation;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 
@@ -26,6 +30,16 @@ public class SunView extends View {
     private int mWidth;
     private int mTop;
 
+    private float mCurrentTime;
+    private float mAngle;
+    private final static String START_TIME = "06:00";
+    private final static String END_TIME = "20:00";
+    private final static String  TAG = "SunView";
+
+    private Bitmap mBitmap;
+    private float mPositionX;
+    private float mPositionY;
+    private final static float mIconRadius = 20;
 
     public SunView(Context context) {
         this(context,null);
@@ -40,16 +54,75 @@ public class SunView extends View {
         intView(context, attrs);
     }
 
+    public void setTime(String time){
+        mCurrentTime = transTime(time);
+        int start = transTime(START_TIME);
+        int end = transTime(END_TIME);
+        mAngle = (mCurrentTime - start)/ (end - start) * 180;
+        Log.d(TAG,"mCurrentTime:" + mCurrentTime + " start:" + start + " end:" + end + " mAngle:" + mAngle);
+        startAnimation(0,mAngle,2000);
+    }
+
+    private void startAnimation(float startAngle, float currentAngle, int duration) {
+        ValueAnimator sunAnimator = ValueAnimator.ofFloat(startAngle, currentAngle);
+        sunAnimator.setDuration(duration);
+        sunAnimator.setTarget(currentAngle);
+        sunAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener()
+        {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation)
+            {
+                //每次要绘制的圆弧角度
+                mAngle = (float) animation.getAnimatedValue();
+                invalidateView();
+            }
+
+        });
+        sunAnimator.start();
+    }
+
+    private void invalidateView() {
+        mPositionX = mWidth / 2 - (float) (mCricleRadius * Math.cos(mAngle * Math.PI / 180)) - mIconRadius;
+        mPositionY =  mTop + mCricleRadius - (float) (mCricleRadius * Math.sin(mAngle * Math.PI / 180) + 15);
+        Log.d(TAG,"mPositionX:" + mPositionX + " mPositionY" + mPositionY);
+        invalidate();
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+        //画圆
         drawCricle(canvas);
+        canvas.save();
+        //画文字
+        drawText(canvas);
+        //画太阳
+        drawSun(canvas);
+        //画太阳走过的圆弧
+        drawOld(canvas);
+
+        super.onDraw(canvas);
+    }
+
+    private void drawOld(Canvas canvas) {
+        mPaint.setColor(mCurrentCricleColor);
+        canvas.drawArc(mRectF,180,mAngle,false,mPaint);
+    }
+
+    private void drawSun(Canvas canvas) {
+        canvas.drawBitmap(mBitmap,mPositionX,mPositionY,mPaint);
+    }
+
+    private void drawText(Canvas canvas) {
+        mPaint.setColor(mFontColor);
+        mPaint.setTextSize(mFontSize);
+        canvas.drawText(START_TIME,mWidth/2 - mCricleRadius,mTop + mCricleRadius + 50,mPaint);
+        canvas.drawText(END_TIME,mWidth/2 + mCricleRadius - 150,mTop + mCricleRadius + 50,mPaint);
     }
 
     private void drawCricle(Canvas canvas) {
         mRectF = new RectF(mWidth/2 - mCricleRadius, mTop, mWidth/2 + mCricleRadius, 2*mCricleRadius + mTop);
         mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setDither(true);
+        mPaint.setDither(true); //防抖动
         mPaint.setColor(mCircleColor);
         canvas.drawArc(mRectF,180,180,true,mPaint);
     }
@@ -65,6 +138,8 @@ public class SunView extends View {
         wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
         mWidth = wm.getDefaultDisplay().getWidth();
         mTop = wm.getDefaultDisplay().getHeight() / 6;
+        mPositionX = mWidth/2 - mCricleRadius - mIconRadius;
+        mPositionY = mCricleRadius + mTop - mIconRadius;
     }
 
     private void intView(Context context, AttributeSet attrs) {
@@ -72,7 +147,7 @@ public class SunView extends View {
         mCircleColor = array.getColor(R.styleable.SunAnimationView_circle_color,
                 getContext().getResources().getColor(R.color.blue));
         mCurrentCricleColor = array.getColor(R.styleable.SunAnimationView_current_color,
-                getContext().getResources().getColor(R.color.yellow));
+                getContext().getResources().getColor(R.color.red));
         mCricleRadius = array.getInteger(R.styleable.SunAnimationView_circle_radius,
                 200);
         mFontSize = (int) array.getDimension(R.styleable.SunAnimationView_font_size,20);
@@ -81,5 +156,22 @@ public class SunView extends View {
         array.recycle();
 
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        //压缩图片
+        BitmapFactory.Options newOpts =  new  BitmapFactory.Options();
+        newOpts.inSampleSize = 3;
+        mBitmap = BitmapFactory.decodeResource(getResources(),R.drawable.sun,newOpts);
+    }
+
+    //将时间转换为分钟
+    private int transTime(String time){
+        int value = 0;
+        if (time == null){
+            return value;
+        }
+        String[] s = time.split(":");
+        int hour = Integer.parseInt(s[0]);
+        int minute = Integer.parseInt(s[1]);
+        value = hour * 60 + minute;
+        return value;
     }
 }
